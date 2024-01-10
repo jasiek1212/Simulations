685203eb258d69b92eaa2c7ca35d1a291b8b4ba1
mbrzezawski <mbrzezawski3@gmail.com>
2024-01-10 14:14:33 +0100

    Wariacje mapy, roślinność

--- Simulations/src/main/java/Project/Model/Core/SimulationConfig.java
+++ Simulations/src/main/java/Project/Model/Core/SimulationConfig.java
@@ -9,16 +9,28 @@ public class SimulationConfig {
     private final int dailyEnergy;
     private final int energyFromPlant;
 
-    public SimulationConfig(Vector2d mapDimensions, int animalsNum, int genomeLength, int startingEnergy, int dailyEnergy, int energyFromPlant){
+    private final int numberOfPlants;
+
+    private final int mapVariant;
+
+    public SimulationConfig(Vector2d mapDimensions, int animalsNum, int genomeLength, int startingEnergy, int dailyEnergy, int energyFromPlant, int numberOfPlants, int mapVariant){
         this.mapDimensions = mapDimensions;
         this.animalsNum = animalsNum;
         this.genomeLength = genomeLength;
         this.startingEnergy = startingEnergy;
         this.dailyEnergy = dailyEnergy;
         this.energyFromPlant = energyFromPlant;
+        this.numberOfPlants = numberOfPlants;
+        this.mapVariant = mapVariant;
+
     }
 
-
+    public int getNumberOfPlants() {
+        return numberOfPlants;
+    }
+    public int getMapVariant() {
+        return mapVariant;
+    }
     public Vector2d getMapDimensions() {
         return mapDimensions;
     }
